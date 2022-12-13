128bb4d7b5cafc1f500586cfe46f227c59d3a9d1
Lars Schöbitz <lars@lse.de>
2022-12-13 11:14:01 +0100

    Add documentation template

---
+++ R/fsmglobal.R
@@ -0,0 +1,13 @@
+#' my_data
+#'
+#' My desc.
+#'
+#' @format A data frame with 10 rows and 3 variables:
+#' \describe{
+#'   \item{ name }{  character }
+#'   \item{ age }{  numeric }
+#'   \item{ weight }{  numeric }
+#' }
+#' @source My source
+#'
+"my_data"
