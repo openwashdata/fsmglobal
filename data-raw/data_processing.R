c7e52823373f9bba852a767f04df987caf946f25
Lars Schöbitz <lars@lse.de>
2023-03-06 13:10:14 +0100

    Import data

--- data-raw/data_processing.R
+++ data-raw/data_processing.R
@@ -1,3 +1,17 @@
+
+
+
+# packages ----------------------------------------------------------------
+
+library(tidyverse)
+
+# data import -------------------------------------------------------------
+
+
+
+
+
 ## code to prepare `DATASET` dataset goes here
 
 usethis::use_data(DATASET, overwrite = TRUE)
+
